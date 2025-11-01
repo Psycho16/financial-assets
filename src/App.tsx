0970c62566b54d25a19d2a56d2769b523ea9e84e
Konstantin <lobanov2000k@yandex.ru>
2025-11-01 13:51:52 +0500

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,48 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import { AssetsPie } from './components/AssetsPie'
 import { UnifiedAssetsList } from './components/UnifiedAssetsList'
 import { ThemeToggle } from './components/ThemeToggle'
 import { AppThemeProvider } from './contexts/ThemeContext'
 
-import { Button, Box } from '@mui/material'
+import { Button, Box, Snackbar } from '@mui/material'
 import { AddAssetsDialog } from './components/AddAssetsDialog'
+import { axiosClient, PATHS } from './utils/axios'
+import { BrowserRouter, useLocation, useSearchParams } from 'react-router-dom';
 
 function AppContent() {
   const [isAddingModalOpen, setIsAddingModalOpen] = useState(false)
+  const [showVerifyFinishAlert, setShowVerifyFinishAlert] = useState(false)
+  const location = useLocation()
 
   const onCloseModal = () => {
     setIsAddingModalOpen(false)
   }
 
+  const register = async () => {
+    const resp = await axiosClient.post(PATHS.USERS.SIGN_UP, { email: "kondtantin.lobanov2000@gmail.com", password: "12345678" })
+    console.log('resp', resp)
+  }
+
+  useEffect(() => {
+    console.log("location", location)
+    const { search } = location
+    const params = new URLSearchParams(search)
+    const token = params.get('hash')
+    console.log("location token", location.hash.startsWith("#access_token="))
+    const shouldShowSnackBar = location.hash.startsWith("#access_token=")
+    setShowVerifyFinishAlert(shouldShowSnackBar)
+  }, [])
+
   return (
     <div>
+      <Snackbar
+        open={showVerifyFinishAlert}
+        autoHideDuration={6000}
+        onClose={() => setShowVerifyFinishAlert(false)}
+        message="Вы успешно подтвердили свой email адрес"
+      // action={action}
+      />
       <header>
         <Box display="flex" justifyContent="space-between" alignItems="center">
           <h1>Мои активы</h1>
@@ -28,6 +54,8 @@ function AppContent() {
             >
               Добавить актив
             </Button>
+            <Button onClick={register}>Зарегистрировать</Button>
+            { }
           </Box>
         </Box>
       </header>
@@ -45,7 +73,9 @@ function AppContent() {
 function App() {
   return (
     <AppThemeProvider>
-      <AppContent />
+      <BrowserRouter>
+        <AppContent />
+      </BrowserRouter>
     </AppThemeProvider>
   )
 }
